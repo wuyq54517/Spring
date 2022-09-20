4c1d8bd5a03c2bfb3e371204a9370970d86c1d3d
wuyq54517 <1275337365@qq.com>
2022-09-20 17:26:16 +0800

    third commit

--- git-test/src/main/java/com/wuyq54517/GitTest.java
+++ git-test/src/main/java/com/wuyq54517/GitTest.java
@@ -6,6 +6,7 @@ package com.wuyq54517;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
